061960c0b4cf12da8328a0a988852b7ad4ce9ed6
liam-leny <liamleny@protonmail.com>
2022-09-09 16:54:36 +0200

    add CHECK

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,10 +1,14 @@
 CREATE TABLE IF NOT EXISTS User(
    lname            TEXT         NOT NULL,
    fname            TEXT         NOT NULL,
-   birthdate        TEXT         NOT NULL,
-   sex              CHAR(1)      NOT NULL,
-   height           INT          NOT NULL,
-   weight           INT          NOT NULL,
+   birthdate        TEXT         NOT NULL
+   CHECK (birthdate < DATE('now')),
+   sex              CHAR(1)      NOT NULL
+   CHECK (sex = 'M' OR sex = 'F'),
+   height           INT          NOT 
+   CHECK (height >= 0 AND height <= 250),
+   weight           INT          NOT NULL
+   CHECK (weight >= 0 AND weight <= 150),
    email            TEXT         NOT NULL       PRIMARY KEY,
    password         TEXT         NOT NULL
 );
@@ -23,7 +27,8 @@ CREATE TABLE IF NOT EXISTS Activity(
 
 CREATE TABLE IF NOT EXISTS DataActivity(
     time                    TEXT         NOT NULL       PRIMARY KEY,
-    cardio_frequency        INT          NOT NULL,
+    cardio_frequency        INT          NOT NULL
+    CHECK (cardio_frequency >= 0 AND cardio_frequency <= 226),
     latitude                REAL         NOT NULL,   
     longitude               REAL         NOT NULL,
     altitude                INT          NOT NULL,
