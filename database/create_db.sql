2e82fb60b5386b33d5dd5079efc57a3512d7339f
Augustin Pasquier <pasquieraugustin@outlook.fr>
2022-09-09 17:08:04 +0200

    complete db

--- database/create_db.sql
+++ database/create_db.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS User(
    CHECK (birthdate < DATE('now')),
    sex              CHAR(1)      NOT NULL
    CHECK (sex = 'M' OR sex = 'F'),
-   height           INT          NOT 
+   height           INT          NOT NULL
    CHECK (height >= 0 AND height <= 250),
    weight           INT          NOT NULL
    CHECK (weight >= 0 AND weight <= 150),
@@ -29,12 +29,9 @@ CREATE TABLE IF NOT EXISTS DataActivity(
     time                    TEXT         NOT NULL       PRIMARY KEY,
     cardio_frequency        INT          NOT NULL
     CHECK (cardio_frequency >= 0 AND cardio_frequency <= 226),
-    latitude                REAL         NOT NULL
-    CHECK (latitude >= -90 AND latitude <= 90),   
-    longitude               REAL         NOT NULL
-    CHECK (longitude >= -180 AND longitude <= 180),
-    altitude                INT          NOT NULL
-    CHECK (altitude >= 0 AND altitude <= 8848),
+    latitude                REAL         NOT NULL,   
+    longitude               REAL         NOT NULL,
+    altitude                INT          NOT NULL,
     CONSTRAINT fk_DataActivity
     FOREIGN KEY (time)
     REFERENCES Activity(id)
