a122030d934cc1cfbf2f280dac9e4a1847da5914
liam-leny <liamleny@protonmail.com>
2022-09-20 19:31:55 +0200

    The connection to the database works

--- php/SqliteConnection.php
+++ php/SqliteConnection.php
@@ -1,19 +1,25 @@
 <?php
 class SqliteConnection {
+    private static SqliteConnection $connexion;
 
+    public static function getInstance(): SqliteConnection {
+        if(!isset(self::$connexion)) {
+            self::$connexion= new SqliteConnection();
+        }
+        return self::$connexion;
+    }
 
     public function getConnection(){
         
         /* Connexion à une base MySQL avec l'invocation de pilote */
-        $dsn = 'mysql:dbname=sport-track;host=127.0.0.1';
-        $user = '';
-        $password = '';
+        $dsn = 'sqlite:..\database\sport_track.db';
 
-        $dbh = new PDO($dsn, $user, $password);
-
+        $db = new PDO($dsn);
+        // Permettre à l'API PDO de pouvoir lancer des exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Pouvoir accéder à la bdd dans les autres méthodes
+        return $db;
 
     }
-
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 ?>
