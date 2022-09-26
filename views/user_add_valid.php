f2b1076dccbd5c3fb3708df65eed30fdaf9247b6
Augustin Pasquier <pasquieraugustin@outlook.fr>
2022-09-26 15:21:12 +0200

    add routes + register form

---
+++ views/user_add_valid.php
@@ -0,0 +1,18 @@
+<?php
+
+include __ROOT__."/views/header.html";
+
+echo "Lastname =". $data['lastname'];
+echo "<br>";
+echo "Firstname =". $data['firstname'];
+
+$lname = data['lname'];
+$fname = data['fname'];
+$birthdate = data['birthdate'];
+$sex = data['sex'];
+$height = data['height'];
+$weight = data['weight'];
+$email = data['email'];
+$password = data['password'];
+
+?>
