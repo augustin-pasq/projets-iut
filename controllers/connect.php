8ceba9242dda960b5cd9c281afd4903068461a62
liam-leny <liamleny@protonmail.com>
2022-09-26 15:32:42 +0200

    Add Login controller

--- controllers/connect.php
+++ controllers/connect.php
@@ -4,11 +4,28 @@ require(__ROOT__.'/controllers/Controller.php');
 class ConnectController extends Controller{
 
     public function get($request){
-        $this->render('connect_form',[]);
+        $this->render('user_connect_form',[]);
     }
 
     public function post($request){
-        $this->render('connect_info',['firstname' => $request['firstname'], 'lastname' => $request['lastname']]);
+        $this->render('user_connect_form',['email' => $request['email'], 'email' => $request['email']]);
+
+
+        $email = $data['email'];
+        $password = $data['password'];
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        $dbc = SqliteConnection::getInstance()->getConnection();
+        $query = "SELECT email FROM User WHERE email = '$email' AND password = '$password'";
+        $stmt = $dbc->prepare($query);
+        $stmt->execute();
+
+        $ArrayEmail = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($ArrayEmail.count() == 1) {
+            echo "Connexion réussi";
+        } else {
+            echo "Le mot de passe et/ou le mot de passe est erronée. Veuillez retenter.";
+        }
     }
 }
 
---
+++ views/user_connect_valid.php
@@ -0,0 +1,4 @@
+<?php include __ROOT__."/views/header.html"; ?>
+
+<h1>Vous êtes bien connecté à l'application</h1>
+
