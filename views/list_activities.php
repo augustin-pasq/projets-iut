8b8e31574381f84489fcc023a0773c4cbd0e516e
liam-leny <liamleny@protonmail.com>
2022-09-28 15:58:19 +0200

    Prevent access to site pages without login

--- views/list_activities.php
+++ views/list_activities.php
@@ -1,3 +1,12 @@
-<?php include __ROOT__."/views/header.html"; ?>
+<?php include __ROOT__."/views/header.html"; 
+if( $_SESSION["id"] == null) {
+    header("location:connect");
+    exit();
+}
+
+?>
+
+
 
 <h1>Liste des activités</h1>
+
