97ee19c23fb9601af3b24bb85cbe618242f1c802
liam-leny <liamleny@protonmail.com>
2022-10-17 17:52:54 +0200

    Attempting to solve synchronisation problems

--- sport-track-db/sport-track-db-test.js
+++ sport-track-db/sport-track-db-test.js
@@ -1,47 +1,63 @@
 var user_dao = require('./sport-track-db').user_dao;
 var db = require('./sqlite_connection').db;
 
-function funcVideTable(callback) {
-    db.run('DELETE FROM User')
-}
 
-function test () {
+// Permet de vider la table User pour pouvoir réexecuter deux fois les tests sans avoir de problèle de clé primaire
+function begintest() {
+    db.run('DELETE FROM User;')
+    affichageTable();
+}   
 
 
-    // Affichage des nom des tables de la base de données pour vérifier la bonne connexion à la base
+// Affichage des nom des tables de la base de données pour vérifier la bonne connexion à la base
+function affichageTable() {
     console.log("[+] Test de la connexion :\t");
     db.all("SELECT name FROM sqlite_master WHERE type='table';", (err, data) => {
         if (err)
             throw err
         console.log(data)
+        testInsert();
     })
-    console.log("\n\n");
+}
 
-    // Insertion de données puis on les affiche pour vérifier la bonne insertion
-    console.log("[+] Test de l'insertion :\t");
+// Insertion de données puis on les affiche pour vérifier la bonne insertion
+function testInsert() {
     var utilisateur = ["John", "Doe", "12/01/1983", "M", 190, 95, "johndoe@test.com", "pass"];
-    user_dao.insert(utilisateur);
-
-    user_dao.findAll(console.log)
-
-    // Modification de données puis on les affiche pour vérifier la bonne modification
-    console.log("[+] Test de la modification :\t");
-    var utilisateurModif = ["John", "Doe", "12/01/1983", "M", 190, 90];
-    var key = "johndoe@test.com"
-    user_dao.update(key, utilisateurModif);
-
-    user_dao.findAll(console.log)
-    
-    // Suppression de données puis on affiche la table pour vérifier la bonne suppression
-    console.log("[+] Test de la suppression :\t");
-    var key = ["johndoe@test.com"]
-    user_dao.delete(key);
-
-    user_dao.findAll(console.log)
-
-
+    console.log("[+] Test de l'insertion :\t");
+    user_dao.insert(utilisateur, () => {
+        console.log("Passage critique")
+        user_dao.findAll((err, res) => {
+            console.log(err, res);
+            testUpdate();
+        })
+    });
 }
 
 
-funcVideTable(test())
 
+// Modification de données puis on les affiche pour vérifier la bonne modification
+function testUpdate() {
+    var utilisateurModif = ["John", "Doe", "12/01/1983", "M", 190, 90];
+    console.log("[+] Test de la modification :\t");
+    var key = "johndoe@test.com"
+    user_dao.update(key, utilisateurModif, () => {
+        user_dao.findAll((err, res) => {
+            console.log(err, res);
+            testDelete();
+        })
+    });
+}
+
+// Suppression de données puis on affiche la table pour vérifier la bonne suppression
+function testDelete() {
+    console.log("[+] Test de la suppression :\t");
+    var key = ["johndoe@test.com"]
+    user_dao.delete(key, () => {
+        user_dao.findAll((err, res) => {
+            console.log(err, res);
+        })
+    });
+}
+
+
+begintest()
