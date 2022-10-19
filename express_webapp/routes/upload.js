dabb8e754a722e21ed09ebabfe3d7096859df736
Augustin Pasquier <pasquieraugustin@outlook.fr>
2022-10-19 23:12:30 +0200

    delete remaining console.log

--- express_webapp/routes/upload.js
+++ express_webapp/routes/upload.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log(req.session.user)
   res.render('upload_activity_form');
 });
 
