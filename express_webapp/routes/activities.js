e3de689e150faae3ace19d30406e032ff2133785
liam-leny <liamleny@protonmail.com>
2022-10-20 17:57:41 +0200

    Advance on activity upload

--- express_webapp/routes/activities.js
+++ express_webapp/routes/activities.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var activity_entry_dao = require('sport-track-db').activity_entry_dao;
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   if (req.session.userID == undefined) { res.redirect('/') }
   else {
-    res.render('list_activities');
+    data = activity_entry_dao.findByKey(req.session.userID)
+    res.render('list_activities', data);
   }
 });
 
--- express_webapp/routes/upload.js
+++ express_webapp/routes/upload.js
@@ -1,5 +1,7 @@
 var express = require('express');
-var formidable = require('formidable');
+var activity_entry_dao = require('sport-track-db').activity_entry_dao;
+var db = require('sport-track-db').db;
+const fileUpload = require('express-fileupload');
 var router = express.Router();
 
 router.get('/', async function (req, res, next) {
@@ -9,18 +11,54 @@ router.get('/', async function (req, res, next) {
   }
 });
 
+router.use(fileUpload());
+
 router.post('/', function (req, res) {
-  var form = new formidable.IncomingForm();
-  form.multiples = true;
-  form.maxFileSize = 50 * 1024 * 1024; // 5MB
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    console.log(files)
-    res.json({ fields, files });
-  });
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  sampleFile = req.files.activity;
+  var data = JSON.parse(sampleFile.data);
+
+  query = "SELECT * FROM Activity WHERE activityUser='" + req.session.userID + "' ORDER BY Activity.rowid DESC;";
+  activities =  new Promise(function (resolve, reject) {
+    db.run(query,
+        function (err) {
+            if (err) reject(err.message)
+            else resolve(true)
+        })
+  })
+
+  date = data["activity"]["date"]
+  description = data["activity"]["description"]
+  time = data["data"][0]["time"],
+  activityUser = req.session.userID,
+  basicData = [date, time, activityUser]
+
+  var idActivity = 1; // TO DETERMINE
+  /*
+  values = [date, description]
+  query = "SELECT ROWID FROM Activity WHERE date = ?, description = ?";
+  id = new Promise(function (resolve, reject) {
+    db.run(query, values,
+        function (err) {
+            if (err) reject(err.message)
+            else resolve(true)
+        })
+      })
+    */
+
+  for (let i = 0; i < data["data"].length; i++) {
+    let time = data["data"][i]["time"]
+    let cardio = data["data"][i]["cardio_frequency"]
+    let latitude = data["data"][i]["latitude"]
+    let longitude = data["data"][i]["longitude"]
+    let altitude = data["data"][i]["altitude"]
+    let donnee = [time, cardio, latitude, longitude, altitude, idActivity]
+    activity_entry_dao.insert(donnee);
+  }
 
 });
 
