304b61f8502826432d5867fd66ae011b7a3812df
liam-leny <liamleny@protonmail.com>
2022-10-20 11:49:53 +0200

    Work on uploading a file

--- express_webapp/routes/upload.js
+++ express_webapp/routes/upload.js
@@ -1,11 +1,27 @@
 var express = require('express');
+var formidable = require('formidable');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', async function (req, res, next) {
   if (req.session.userID == undefined) { res.redirect('/') }
   else {
     res.render('upload_activity_form');
   }
+});
+
+router.post('/', function (req, res) {
+  var form = new formidable.IncomingForm();
+  form.multiples = true;
+  form.maxFileSize = 50 * 1024 * 1024; // 5MB
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(files)
+    res.json({ fields, files });
+  });
+
 });
 
 module.exports = router;
