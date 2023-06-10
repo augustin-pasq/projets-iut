eee71724f40d44fd74723f2580ddaede1a710688
Augustin Pasquier <pasquieraugustin@outlook.fr>
2023-06-10 15:08:18 +0200

    app commit

---
+++ app/src/pages/_app.js
@@ -0,0 +1,7 @@
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
