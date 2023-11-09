109e585d5fe911876d97f6d5ec6d7313651f09d1
Augustin Pasquier <pasquieraugustin@outlook.fr>
2023-11-09 11:27:26 +0100

    Handling empty username + style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,22 @@
-import React, {useState} from "react"
+import React, {useRef, useState} from "react"
 import {Button} from "primereact/button"
 import {Dialog} from "primereact/dialog"
 import {InputText} from "primereact/inputtext"
-import {InputNumber} from "primereact/inputnumber";
+import {InputNumber} from "primereact/inputnumber"
+import {Toast} from "primereact/toast"
+import {Card} from "primereact/card"
 
 export default function Home() {
     const [username, setUsername] = useState("")
     const [roundsToReach, setRoundsToReach] = useState(2)
     const [accessCode, setAccessCode] = useState("")
     const [isGameCreated, setIsGameCreated] = useState({state: false, owner: false})
-    const [isGameStarted, setIsGameStarted] = useState(true)
+    const [isGameStarted, setIsGameStarted] = useState(false)
     const [openModal, setOpenModal] = useState(false)
+    const toast = useRef(null)
 
     const handleDisplayModal = async () => {
-        if (username !== "") {
+        if (username.trim() !== "") {
             const results = await (await fetch("/api/createUser", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
@@ -26,7 +29,11 @@ export default function Home() {
                     .then(() => setOpenModal(true))
             }
         } else {
-            // TODO: handle empty username
+            toast.current.show({
+                severity: "error",
+                summary: "Eh, toi là !",
+                detail: "Tu dois entrer un pseudo pour jouer."
+            })
         }
     }
 
@@ -84,19 +91,24 @@ export default function Home() {
         <div className="container">
             <section id="deck-container">
                 {isGameStarted ?
-                        <>
-                            <div className="card card-reversed">
+                    <>
+                        <div className="card card-reversed">
 
-                            </div>
-                        </>
+                        </div>
+                    </>
                     :
+                    <Card className="game-launcher-card">
                         <div className="game-launcher">
+                            <h1>Punto</h1>
+
                             <div>
                                 <label htmlFor="username-field">Choisis un pseudo :</label>
-                                <InputText id="username-field" aria-describedby="username-help" placeholder="Pseudo stylé" onChange={(e) => setUsername(e.target.value)} />
+                                <InputText id="username-field" aria-describedby="username-help" maxLength={32}
+                                           placeholder="Pseudo stylé" onChange={(e) => setUsername(e.target.value)}/>
                             </div>
                             <Button label="Jouer !" onClick={handleDisplayModal}/>
                         </div>
+                    </Card>
                 }
 
             </section>
@@ -106,41 +118,54 @@ export default function Home() {
                     {[...Array(6)].map((_, i) => (
                         <div id={`row-${i}`} key={`row-${i}`} className="row">
                             {[...Array(6)].map((_, j) => (
-                                <div id={`cell-${j}`} key={`cell-${j}`} className="cell" onClick={() => addCard(i, j)}></div>
+                                <div id={`cell-${j}`} key={`cell-${j}`} className="cell"
+                                     onClick={() => isGameStarted && addCard(i, j)}></div>
                             ))}
                         </div>
                     ))}
                 </div>
             </section>
 
-            <Dialog header="Jouer au Punto" visible={openModal} dismissableMask={true} style={{ width: '60vw' }} onHide={() => setOpenModal(false)} draggable={false}>
+            <Dialog header="Jouer au Punto" visible={openModal} dismissableMask={true} style={{width: '60vw'}}
+                    onHide={() => setOpenModal(false)} draggable={false}>
                 <div className="modal-container">
                     <div className="game-creation">
                         <h3>Créer une partie</h3>
                         <label htmlFor="rounds-to-reach">Nombre de manche à atteindre pour remporter la partie :</label>
-                        <InputNumber id="rounds-to-reach" value={roundsToReach} onValueChange={(e) => setRoundsToReach(e.value)} showButtons buttonLayout="horizontal" step={1}
-                                     decrementButtonClassName="p-button-danger" incrementButtonClassName="p-button-success" incrementButtonIcon="pi pi-plus" decrementButtonIcon="pi pi-minus" min={1} disabled={isGameCreated.state}/>
-                        {!isGameCreated.state && <Button className="mt-5 mb-3" label="Créer une partie" onClick={() => handleGameLaunch("created")}/>}
+                        <InputNumber id="rounds-to-reach" value={roundsToReach}
+                                     onValueChange={(e) => setRoundsToReach(e.value)} showButtons
+                                     buttonLayout="horizontal" step={1}
+                                     decrementButtonClassName="p-button-danger"
+                                     incrementButtonClassName="p-button-success" incrementButtonIcon="pi pi-plus"
+                                     decrementButtonIcon="pi pi-minus" min={1} disabled={isGameCreated.state}/>
+                        {!isGameCreated.state && <Button className="mt-5 mb-3" label="Créer une partie"
+                                                         onClick={() => handleGameLaunch("created")}/>}
                     </div>
                     <div className="game-join">
                         <h3>Rejoindre une partie</h3>
 
                         {isGameCreated.state ?
-                                <>
-                                    <span>Partage ce code avec tes amis pour jouer avec eux :</span>
-                                    <span className="access-code">{accessCode}</span>
-                                    {isGameCreated.owner && <Button className="mt-5 mb-3" label="Lancer la partie" onClick={() => startGame()}/>}
-                                </>
+                            <>
+                                <span>Partage ce code avec tes amis pour jouer avec eux :</span>
+                                <span className="access-code">{accessCode}</span>
+                                {isGameCreated.owner && <Button className="mt-5 mb-3" label="Lancer la partie"
+                                                                onClick={() => startGame()}/>}
+                            </>
                             :
-                                <>
-                                    <label htmlFor="access-code-feld">Entre le code que tu as reçu pour jouer avec tes amis :</label>
-                                    <InputText id="access-code-field" aria-describedby="username-help" onChange={(e) => setAccessCode(e.target.value.toUpperCase())} />
-                                    <Button className="mt-5 mb-3" label="Rejoindre une partie" onClick={() => handleGameLaunch("joined")}/>
-                                </>
+                            <>
+                                <label htmlFor="access-code-feld">Entre le code que tu as reçu pour jouer avec tes amis
+                                    :</label>
+                                <InputText id="access-code-field" aria-describedby="username-help"
+                                           onChange={(e) => setAccessCode(e.target.value.toUpperCase())}/>
+                                <Button className="mt-5 mb-3" label="Rejoindre une partie"
+                                        onClick={() => handleGameLaunch("joined")}/>
+                            </>
                         }
                     </div>
                 </div>
             </Dialog>
+
+            <Toast ref={toast} position="bottom-left"/>
         </div>
-    );
+    )
 }
